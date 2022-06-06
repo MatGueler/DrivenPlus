0330a320be1c0377112efce85fdfc7b957354388
MattGueler <mateus.gueler@gmail.com>
2022-06-06 17:24:06 -0300

    Posta novo usuário na API

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,25 +5,46 @@ import { Button } from '../Button/Button';
 import { Container } from '../Container/Container';
 import { Main, Logo } from '../Login/Login-style';
 import { Input } from '../Input/Input'
+import { useState } from 'react';
+import axios from 'axios';
+
+
 
 function Login() {
-        let navigate = useNavigate()
-        return (
+    let navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-            <Container>
-                <Main>
-                    <Logo>
-                        <h1>Driven <GoPlus size={50} color='#FF4791' /></h1>
-                    </Logo>
-                    <form>
-                        <Input placeholder='E-mail' />
-                        <Input placeholder='Senha' />
-                        <Button>ENTRAR</Button>
-                    </form>
-                    <p onClick={() => navigate('/sign-up')}>Não possuí uma conta? Cadastre-se</p>
-                </Main>
-            </Container>
-        );
+    function signUp(event) {
+        event.preventDefault();
+        const body = {
+            email,
+            password
+        }
+        const URL = 'https://mock-api.driven.com.br/api/v4/driven-plus/auth/login'
+
+        const promise = axios.post(URL, body)
+
+        promise.then(() => { navigate('/subscriptions') })
+            .catch((err) => console.log(err))
     }
+
+    return (
+
+        <Container>
+            <Main>
+                <Logo>
+                    <h1>Driven <GoPlus size={50} color='#FF4791' /></h1>
+                </Logo>
+                <form onSubmit={signUp}>
+                    <Input placeholder='E-mail' onChange={(e) => { setEmail(e.target.value) }} value={email} />
+                    <Input placeholder='Senha' onChange={(e) => { setPassword(e.target.value) }} value={password} />
+                    <Button>ENTRAR</Button>
+                </form>
+                <p onClick={() => navigate('/sign-up')}>Não possuí uma conta? Cadastre-se</p>
+            </Main>
+        </Container>
+    );
+}
 
 export default Login;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,22 +1,49 @@
-import '../Login/Login-style'
-import { Button } from '../Button/Button';
+import { Container } from '../Container/Container';
 import { useNavigate } from "react-router-dom";
 import { Main } from '../Login/Login-style';
+import { Button } from '../Button/Button';
 import { Input } from '../Input/Input';
-import { Container } from '../Container/Container';
+import { useState } from 'react';
+import '../Login/Login-style'
+import axios from 'axios';
+
 
 function Register() {
     let navigate = useNavigate();
+    const [name, setName] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function Create(body) {
+        const URL = 'https://mock-api.driven.com.br/api/v4/driven-plus/auth/sign-up'
+        const promise = axios.post(URL, body)
+
+        promise.then((response) => { console.log(response) })
+            .catch((err) => { console.log(err) })
+    }
+
+    function User(event) {
+        event.preventDefault();
+        const body = {
+            email,
+            name,
+            cpf,
+            password
+        }
+        Create(body)
+    }
+
     return (
 
         <Container>
             <Main>
-                <form>
-                    <Input placeholder='Nome' />
-                    <Input placeholder='CPF' />
-                    <Input placeholder='E-mail' />
-                    <Input placeholder='Senha' />
-                    <Button>CADASTRAR</Button>
+                <form onSubmit={User}>
+                    <Input type='text' placeholder='Nome' onChange={(e) => { setName(e.target.value) }} value={name} />
+                    <Input type='text' placeholder='CPF' onChange={(e) => { setCpf(e.target.value) }} value={cpf} />
+                    <Input type='email' placeholder='E-mail' onChange={(e) => { setEmail(e.target.value) }} value={email} />
+                    <Input type='password' placeholder='Senha' onChange={(e) => { setPassword(e.target.value) }} value={password} />
+                    <Button type='submit'>CADASTRAR</Button>
                 </form>
                 <p onClick={() => navigate('/')}>Já possuí uma conta? Entre</p>
             </Main>
